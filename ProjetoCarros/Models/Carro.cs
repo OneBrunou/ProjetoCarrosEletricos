2762528c7e75661c5c8dac9ea36a2082d9a5b8e3
Malu <littlefujoshi7574@gmail.com>
2026-06-15 17:05:30 -0300

    Feat: Adicionando a model carro

---
+++ ProjetoCarros/Models/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoCarros.Models
+{
+    public class Carro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        public string? Marca { get; set; }
+        public int Ano { get; set; }
+        public string? Descricao { get; set; }
+        public string? Imagem { get; set; }
+
+    }
+}
