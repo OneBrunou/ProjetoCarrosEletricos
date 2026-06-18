a1676bcd79297e8bfef4188c1d247d6a53625ff7
Henrique Vincenzo <henrimelo06@gmail.com>
2026-06-17 23:07:35 -0300

    feat: adiciona splash screen e logo na navbar

---
+++ ProjetoCarros/wwwroot/js/Splash.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const logo = document.getElementById('splash-logo').querySelector('img');
+    const overlay = document.getElementById('splash-overlay');
+
+    setTimeout(() => {
+        logo.style.transform = 'scale(1)';
+        logo.style.opacity = '1';
+    }, 100);
+
+    setTimeout(() => {
+        overlay.classList.add('hide');
+    }, 2000);
+
+    setTimeout(() => {
+        overlay.remove();
+    }, 2800);
+});
